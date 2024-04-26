2749fa355f4957e1cc55c1a8006ce69045b1fc30
Shivam Kumar Patel <151195034+sp924847@users.noreply.github.com>
2024-04-26 13:28:24 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,24 @@
+
 function longestSubstring(s) {
+    let maximumLen = 0;
+    let left = 0;
+    const charIndexMap = new Map();  
 
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
 
-    // Implementation of longestSubstring function
+         
+        if (charIndexMap.has(currentChar) && charIndexMap.get(currentChar) >= left) {
+            left = charIndexMap.get(currentChar) + 1;
+        }
+
+         charIndexMap.set(currentChar, right);
+
+         maximumLen = Math.max(maximumLen, right - left + 1);
+    }
+
+    return maximumLen;
 }
+
 
 module.exports = { longestSubstring };
